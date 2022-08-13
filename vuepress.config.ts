b4b6d22c27bf7b324790a3935f1eda21d57f568d
Omico <me@omico.icu>
2022-08-12 22:43:02 -0700

    Migrate to VuePress V2

---
+++ vuepress.config.ts
@@ -0,0 +1,24 @@
+import {
+  defaultTheme,
+  defineUserConfig,
+} from 'vuepress';
+
+export default defineUserConfig(
+  {
+    title: '原神萌新笔记',
+    description: '旅行者你将去往何方',
+    theme: defaultTheme(
+      {
+        docsRepo: "Omico/GenshinImpactRookieNotes",
+        docsDir: "docs",
+        docsBranch: "main",
+        editLink: true,
+        editLinkText: "帮助我们改善此页面！",
+        navbar: [
+          { text: "初识原神", link: "/first-acquaintance/" },
+          { text: "指南", link: "/guide/" },
+        ],
+      },
+    ),
+  }
+)
